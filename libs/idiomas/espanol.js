ebea808b3ec0d69b7361c3534d2b54e0e8b791b4
elrebelde21 <107805973+elrebelde21@users.noreply.github.com>
2024-09-01 21:28:47 -0300

    Update espanol.js

--- libs/idiomas/espanol.js
+++ libs/idiomas/espanol.js
@@ -229,7 +229,7 @@ text20: (fb, md) => `╭─────────────┈⊷
 
 * apt update && apt upgrade && pkg update && pkg upgrade && pkg install bash && pkg install libwebp && pkg install git -y && pkg install nodejs -y && pkg install ffmpeg -y && pkg install wget && pkg install imagemagick -y && pkg install yarn
 
-* git clone https://github.com/elrebelde21/NovaBot-MD && cd NovaBot-MD && yarn && npm install
+* git clone https://github.com/elrebelde21/NovaBot_MD && cd NovaBot_MD && yarn && npm install
 
 * npm start
 
